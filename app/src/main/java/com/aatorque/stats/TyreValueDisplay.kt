4132bf7887ebc77a3f0a5c2ea4ec9b6462d6ff13
Arda Celik <celikarda98@gmail.com>
2026-03-15 10:48:32 +0100

    fixed the tyre side somewhat

--- app/src/main/java/com/aatorque/stats/TyreValueDisplay.kt
+++ app/src/main/java/com/aatorque/stats/TyreValueDisplay.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,6 +32,13 @@ class TyreValueDisplay : Fragment() {
     ): View {
         Timber.i("onCreateView")
         binding = FragmentTyreValueBinding.inflate(inflater, container, false)
+        val alignRight = id in RIGHT_ALIGNED_IDS
+        binding.valueElement.textAlignment = if (alignRight) {
+            View.TEXT_ALIGNMENT_VIEW_END
+        } else {
+            View.TEXT_ALIGNMENT_VIEW_START
+        }
+        binding.valueElement.gravity = if (alignRight) Gravity.END else Gravity.START
         settingsViewModel.typefaceLiveData.observe(viewLifecycleOwner, this::setupTypeface)
         return binding.root
     }
@@ -49,5 +57,14 @@ class TyreValueDisplay : Fragment() {
     @SuppressLint("SetTextI18n")
     fun onUpdate(data: TorqueData) {
         binding.value = if (unit.isBlank()) data.lastDataStr else "${data.lastDataStr} $unit"
+    }
+
+    companion object {
+        private val RIGHT_ALIGNED_IDS = setOf(
+            R.id.display3,
+            R.id.display4,
+            R.id.display7,
+            R.id.display8,
+        )
     }
 }
